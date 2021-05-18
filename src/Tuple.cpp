d697b1e06421d43559d3f14f803e2ca637a362aa
Geoff Rosenberg <geoff@cgrosenberg.com>
2021-05-18 12:59:57 -0700

    Add the rest of ch1 tests, but not the "putting it together" section

--- src/Tuple.cpp
+++ src/Tuple.cpp
@@ -1,19 +1,11 @@
 #include "Tuple.h"
 
+#include <cmath>
 #include <cstdlib>
 #include <limits>
+#include <stdexcept>
 
-Tuple::Tuple(double X, double Y, double Z, double W) : x(X), y(Y), z(Z) {
-  if (CompareDoubles(W, 1.0)) {
-    w = true;
-  } else {
-    w = false;
-  }
-}
-
-bool
-Tuple::IsPoint() {
-  return w;
+Tuple::Tuple(double X, double Y, double Z, double W) : x(X), y(Y), z(Z), w(W) {
 }
 
 bool
@@ -23,23 +15,77 @@ CompareDoubles(double a, double b) {
   return true;
 }
 
-Tuple Point(double X, double Y, double Z) {
+Tuple
+Point(double X, double Y, double Z) {
   return Tuple(X, Y, Z, 1.0);
 }
 
-Tuple Vector(double X, double Y, double Z) {
+Tuple
+Vector(double X, double Y, double Z) {
   return Tuple(X, Y, Z, 0.0);
 }
 
-Tuple
-Tuple::operator+(Tuple t1) {
-  // TODO: handle if this is a point and we're adding another point
-  return Tuple(this->x + t1.GetX(), this->y + t1.GetY(), this->z + t1.GetZ(), 0.0);
+bool
+Tuple::IsPoint() const {
+  return CompareDoubles(w, 1.0);
 }
 
 Tuple
-Tuple::operator-(Tuple t1) {
-  // TODO: handle if this is a point and we're adding another point
-  
-  return Tuple(this->x - t1.GetX(), this->y - t1.GetY(), this->z - t1.GetZ(), 0.0);
+Tuple::operator+(const Tuple &t1) const {
+  if (IsPoint() && t1.IsPoint()) {
+    throw std::runtime_error("Cannot add two points.");
+  }
+  return Tuple(this->x + t1.GetX(), this->y + t1.GetY(), this->z + t1.GetZ(), this->IsPoint() + t1.IsPoint());
+}
+
+Tuple
+Tuple::operator-(const Tuple &t1) const {
+  return Tuple(this->x - t1.GetX(), this->y - t1.GetY(), this->z - t1.GetZ(), this->IsPoint() - t1.IsPoint());
+}
+
+bool
+Tuple::operator==(const Tuple &t1) const {
+  if (!CompareDoubles(GetX(), t1.GetX()) ||
+      !CompareDoubles(GetY(), t1.GetY()) ||
+      !CompareDoubles(GetZ(), t1.GetZ()) ||
+      !CompareDoubles(GetW(), t1.GetW())) {
+    return false;
+  }
+
+  return true;
+}
+
+Tuple
+Tuple::operator*(const double d) const {
+  return Tuple(GetX() * d, GetY() * d, GetZ() * d, GetW() * d);
+}
+
+Tuple
+Tuple::operator/(const double d) const {
+  return Tuple(GetX() / d, GetY() / d, GetZ() / d, GetW() / d);
+}
+
+double
+Tuple::Magnitude() const {
+  return std::sqrt(x * x + y * y + z * z);
+}
+
+Tuple
+Tuple::Normalize() const {
+  return Tuple(x / Magnitude(), y / Magnitude(), z / Magnitude(), w);
+}
+
+double
+Dot(const Tuple &t1, const Tuple &t2) {
+  return t1.GetX() * t2.GetX() +
+         t1.GetY() * t2.GetY() +
+         t1.GetZ() * t2.GetZ();
+}
+
+Tuple
+Cross(const Tuple &a, const Tuple &b) {
+  // TODO: ensure and b are vectors.
+  return Vector(a.GetY() * b.GetZ() - a.GetZ() * b.GetY(),
+                a.GetZ() * b.GetX() - a.GetX() * b.GetZ(),
+                a.GetX() * b.GetY() - a.GetY() * b.GetX());
 }
--- test/Ch1-test.cpp
+++ test/Ch1-test.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <cmath>
+#include <stdexcept>
+
 #include "src/Tuple.h"
 
 TEST(ch1tests, construct_tuples) {
@@ -32,25 +35,70 @@ TEST(ch1tests, factory_functions) {
 }
 
 TEST(ch1tests, tuple_addition) {
-  auto v1 = Vector(1, 2, 3);
-  auto v2 = Vector(1, 2, 3);
+  auto a1 = Point(3, -2, 5);
+  auto a2 = Vector(-2, 3, 1);
 
-  auto v3 = v1 + v2;
+  auto a3 = a1 + a2;
 
-  EXPECT_EQ(v3.GetX(), 2);
-  EXPECT_EQ(v3.GetY(), 4);
-  EXPECT_EQ(v3.GetZ(), 6);
-  EXPECT_FALSE(v3.IsPoint());
+  EXPECT_EQ(a3.GetX(), 1);
+  EXPECT_EQ(a3.GetY(), 1);
+  EXPECT_EQ(a3.GetZ(), 6);
+  EXPECT_TRUE(a3.IsPoint());
 }
 
 TEST(ch1tests, tuple_subtraction) {
   auto p1 = Point(3, 2, 1);
   auto p2 = Point(5, 6, 7);
 
-  auto v3 = p1 - p2;
+  EXPECT_EQ(p1 - p2, Vector(-2, -4, -6));
 
-  EXPECT_EQ(v3.GetX(), -2);
-  EXPECT_EQ(v3.GetY(), -4);
-  EXPECT_EQ(v3.GetZ(), -6);
-  EXPECT_FALSE(v3.IsPoint());
+  auto v1 = Vector(5, 6, 7);
+  EXPECT_EQ(p1 - v1, Point(-2, -4, -6));
+
+  auto v2 = Vector(3, 2, 1);
+  EXPECT_EQ(v2 - v1, Vector(-2, -4, -6));
+}
+
+TEST(ch1tests, tuple_negation) {
+  auto zero = Vector(0, 0, 0);
+  auto v = Vector(1, -2, 3);
+
+  EXPECT_EQ(zero - v, Vector(-1, 2, -3));
+}
+
+TEST(ch1tests, tuple_multiplication) {
+  Tuple a(1, -2, 3, -4);
+
+  EXPECT_EQ(a * 3.5, Tuple(3.5, -7, 10.5, -14));
+  EXPECT_EQ(a * 0.5, Tuple(0.5, -1, 1.5, -2));
+  EXPECT_EQ(a / 2, Tuple(0.5, -1, 1.5, -2));
+}
+
+TEST(ch1tests, tuple_magnitude) {
+  EXPECT_EQ(Vector(1, 0, 0).Magnitude(), 1);
+  EXPECT_EQ(Vector(0, 1, 0).Magnitude(), 1);
+  EXPECT_EQ(Vector(1, 0, 0).Magnitude(), 1);
+  EXPECT_EQ(Vector(0, 0, 1).Magnitude(), 1);
+  EXPECT_EQ(Vector(1, 2, 3).Magnitude(), std::sqrt(14));
+  EXPECT_EQ(Vector(-1, -2, -3).Magnitude(), std::sqrt(14));
+}
+
+TEST(ch1tests, tuple_normalization) {
+  EXPECT_EQ(Vector(4, 0, 0).Normalize(), Vector(1, 0, 0));
+  EXPECT_EQ(Vector(1, 2, 3).Normalize(), Vector(1 / std::sqrt(14), 2 / std::sqrt(14), 3 / std::sqrt(14)));
+}
+
+TEST(ch1test, tuple_dotProduct) {
+  auto a = Vector(1, 2, 3);
+  auto b = Vector(2, 3, 4);
+
+  EXPECT_EQ(Dot(a, b), 20);
+}
+
+TEST(ch1test, tuple_crossProduct) {
+  auto a = Vector(1, 2, 3);
+  auto b = Vector(2, 3, 4);
+
+  EXPECT_EQ(Cross(a, b), Vector(-1, 2, -1));
+  EXPECT_EQ(Cross(b, a), Vector(1, -2, 1));
 }
